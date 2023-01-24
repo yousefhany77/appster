9c9af67df15d3661fc85ae39690889c08a827af4
yousef hany <yousefhany2014@gmail.com>
2023-01-25 01:01:30 +0200

    Fixed unstable behavior on logout

--- util/logout.ts
+++ util/logout.ts
@@ -2,13 +2,12 @@ import { clientAuth } from "../firebase";
 
 async function logout() {
   try {
-     await fetch("/api/logout");
-    clientAuth.signOut();
-    window.location.href = "/";
+    await fetch("/api/logout");
+    await clientAuth.signOut();
+    window.location.replace("/");
   } catch (error) {
     console.log(error);
   }
 }
 
 export default logout;
-
